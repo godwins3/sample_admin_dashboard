26eebb748e949c60a8967076838c33f509b48fa6
godwins3 <praisegodwins4@gmail.com>
2024-08-09 12:08:43 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { auth } from '@/auth';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn',
-  description: 'ctn dashboard Dashboard'
+  title: 'CTN ',
+  description: 'ctn Dashboard'
 };
 
 export default async function RootLayout({
